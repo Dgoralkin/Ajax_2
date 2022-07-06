7e9b40749f42ea849430e4d36a451a2771a3c951
Dany Goralkin <Goralkin@Gmail.com>
2022-07-06 21:28:59 +0300

    folderes rearenged .gitignore added

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 
 from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
 
 
 # Configure application
 app = Flask(__name__)
 
-Session(app)
 
 
 
