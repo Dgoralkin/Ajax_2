08321df4eb5352e2ebc724ba6cba573dc600bba1
Dany Goralkin <Goralkin@Gmail.com>
2022-07-06 18:48:15 +0300

    Flask added

---
+++ app.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+
+
+# Configure application
+app = Flask(__name__)
+
+Session(app)
+
+
+
+
+
+# --------------------------------------------------------------------------------- /
+@app.route("/index")
+
+def index():
+    
+    return render_template("index.html")
