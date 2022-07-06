e0ce285f256334e1359d0fe9d68f662ea3be8c08
Dany Goralkin <Goralkin@Gmail.com>
2022-07-06 16:23:24 +0300

    Ajac Script added

--- Script.js
+++ Script.js
@@ -1,2 +1,14 @@
 /// JavaScript File
 
+function show_info(value) {
+    console.log(value);
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        document.getElementById('demo').innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "Dany.html");
+    xhttp.send();
+
+    return;
+}
