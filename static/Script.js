7df3e11e16f8724957b3baffd4eb5cbf8e8a6f73
Dany Goralkin <Goralkin@Gmail.com>
2022-07-08 17:47:29 +0300

    pull commit

--- static/Script.js
+++ static/Script.js
@@ -9,9 +9,7 @@ function show_info(value) {
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            document.getElementById('demo').innerHTML = xhttp.responseText;
-        }
+        document.getElementById('demo').innerHTML = xhttp.responseText;
     };
     xhttp.open("GET", value + '.html', true);
     xhttp.send();
